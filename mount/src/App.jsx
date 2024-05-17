134141f9362122259b69c868f305b34fe70fff04
ZaripovAzizbekk <azizzaripov07@gmail.com>
2024-05-17 21:16:08 +0500

    Mount
    mount

---
+++ mount/src/App.jsx
@@ -0,0 +1,137 @@
+import React from "react"
+import './index.css';
+
+export default function App() {
+  return (
+    <div>
+      <div className='navbar'>
+        <div className="container">
+          <div className="navbar-content">
+            <h1 className="navbar-logo">scoot</h1>
+            <h4 className="navbar-text">About</h4>
+            <h4 className="navbar-text">Location</h4>
+            <h4 className="navbar-text">Careers</h4>
+
+            <button className="navbar-btn">Get Scootin</button>
+
+          </div>
+        </div>
+      </div>
+
+      <div className="header">
+        <div className="container">
+          <div className="header-content">
+            <h2 className="header-text">Scooter sharing <br /> made simple</h2>
+            <p className="header-title">Scoot takes the hassle out of urban mobility. Our bikes <br /> are placed in convenient locations in each of our cities. <br /> Use our app to locate the nearest bike, unlock it with a <br /> tap, and you’re away!</p>
+            <button className="header-btn">Get Scootin</button>
+          </div>
+          <img src="" alt="" />
+        </div>
+      </div>
+
+      <div className="locate">
+        <div className="container">
+          <div className="locate-content">
+            <div className="locate-cards">
+              <img src="" alt="..rasm bor.." />
+              <h3 className="locate-text">Locate with app</h3>
+              <p className="locate-title">Use the app to find the nearest scooter to you. We are continuously placing scooters in the areas with most demand, so one should never be too far away. </p>
+            </div>
+
+            <div className="locate-cards">
+              <img src="" alt="..rasm bor.." />
+              <h3 className="locate-text">Locate with app</h3>
+              <p className="locate-title">Use the app to find the nearest scooter to you. We are continuously placing scooters in the areas with most demand, so one should never be too far away. </p>
+            </div>
+
+            <div className="locate-cards">
+              <img src="" alt="..rasm bor.." />
+              <h3 className="locate-text">Locate with app</h3>
+              <p className="locate-title">Use the app to find the nearest scooter to you. We are continuously placing scooters in the areas with most demand, so one should never be too far away. </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="easy">
+        <div className="container">
+          <div className="easy-content">
+            <div className="easy-cards">
+              <h2 className="easy-text">Easy to use <br /> riding telemetry</h2>
+              <p className="easy-title">The Scoot app is available with riding telemetry. This means it can show you your average speed, how long you've been using the scooter, your traveling distance, and many more things all in an easy to use app.</p>
+              <button className="easy-btn">Learn More</button>
+            </div>
+            <img src="" alt="..rasm bor.." />
+          </div>
+
+
+
+        </div>
+      </div>
+
+      <div className="city">
+        <div className="container">
+          <div className="city-content">
+            <img src="" alt="..rasm bor.." />
+            <div className="city-cards">
+              <h2 className="city-text">Easy to use <br /> riding telemetry</h2>
+              <p className="city-title">The Scoot app is available with riding telemetry. This means it can show you your average speed, how long you've been using the scooter, your traveling distance, and many more things all in an easy to use app.</p>
+              <button className="city-btn">Learn More</button>
+            </div>
+          </div>
+
+
+
+        </div>
+      </div>
+
+      <div className="zero">
+        <div className="container">
+          <div className="zero-content">
+            <div className="zero-cards">
+              <h2 className="zero-text">Easy to use <br /> riding telemetry</h2>
+              <p className="zero-title">The Scoot app is available with riding telemetry. This means it can show you your average speed, how long you've been using the scooter, your traveling distance, and many more things all in an easy to use app.</p>
+              <button className="zero-btn">Learn More</button>
+            </div>
+            <img src="" alt="..rasm bor.." />
+          </div>
+
+
+
+        </div>
+      </div>
+
+      <div className="sign">
+        <div className="container">
+          <div className="sign-content">
+            <h1 className="sign-text">Sign up and <br /> Scoot off today</h1>
+            <div className="sign-img">
+              <img src="" alt="..rasm bor.." />
+              <img src="" alt="..rasm bor.." />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="footer">
+        <div className="container">
+          <div className="footer-content">
+            <div className="footer-texts">
+            <h1 className="footer-text">scoot</h1>
+            <p className="footer-title">About</p>
+            <p className="footer-title">Location</p>
+            <p className="footer-title">Careers</p>
+            </div>
+            <div className="footer-btns">
+            <button className="footer-btn">Log out</button>
+          <button className="footer-btn">Log in</button>
+          <button className="footer-btn2">?</button>
+          </div>
+
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
